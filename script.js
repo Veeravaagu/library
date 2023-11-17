ac94590c5a81484fe5524f3606b340ef5e92ab7c
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-17 17:32:39 +0530

    Remove eventListeners for everything except submit button

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-let title = document.querySelector('.title');
-const h1 = document.querySelector('h1');
-const h2 = document.querySelector('h2');
-const p = document.querySelector('p');
-const h3 = document.querySelector('h3');
+const title = document.querySelector('.title');
 const submit = document.querySelector('#submit');
 const author = document.querySelector('.author');
 const pages = document.querySelector('.pages');
 const read = document.querySelector('.status');
 
-
+console.log(title);
 
 const myLibrary = [];
 
@@ -25,24 +21,17 @@ function Book(title,author,pages,read) {
 function addBookToLibrary() {
 
         submit.addEventListener('click', function (e){
-            console.log(e);
-            console.log(e.target);
+            e.preventDefault();
+            const bookTitle = title.value;
+            const authorName = author.value;
+            const pagesRead = pages.value;
+            const readStatus = read.value;
+
+            console.log(bookTitle);
+            console.log(authorName);
+            console.log(pagesRead);
+            console.log(readStatus);
         })
-        title.addEventListener('keydown',function test (event){
-            h1.innerText += event.key;
-            console.log(h1.innerText)
-        })
-        author.addEventListener('keydown',function test (event){
-            p.innerText += event.key;
-            console.log(p.innerText)
-        })
-        pages.addEventListener('keydown',function test (event){
-            console.log(event.key)
-        })
-        read.addEventListener('click',function test (event){
-            console.log(event.target.checked)
-        })
-        
     }
 addBookToLibrary() 
 
