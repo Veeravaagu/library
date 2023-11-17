1586d28acdfbf8d12f13abdeac34b98a8c8b79ec
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-17 18:42:45 +0530

    Create display function & iterate over the array and display the results

--- script.js
+++ script.js
@@ -20,23 +20,63 @@ function addBookToLibrary(title,author,pages,read) {
     const newBook = new Book(title,author,pages,read);
     myLibrary.push(newBook)
     clearDisplay()
+    display();
     console.log(myLibrary, newBook);
     }
 
 
 submit.addEventListener('click', function (e){
     e.preventDefault();
+    if (title.value !== '' && author.value !== ''){
     const bookTitle = title.value;
     const authorName = author.value;
     const pagesRead = pages.value;
     const readStatus = read.checked;
+
     addBookToLibrary(bookTitle,authorName,pagesRead,readStatus)
+    } else{
+        alert('Fill the Required Fields[Title and Author]')
+    }
 })
 
 function clearDisplay(){
     title.value = ''
     author.value = ''
-    pages.value = ''
+    pages.value = '0'
     read.checked = ''
 }
+
+function display() {
+    const libraryContainer = document.querySelector('.library-container');
+  
+
+    libraryContainer.innerHTML = '';
+  
+    for (const book of myLibrary) {
+      const card = document.createElement('div');
+      card.classList.add('book-card');
+  
+      const titleElement = document.createElement('p');
+      titleElement.textContent = `Title: ${book.title}`;
+      card.appendChild(titleElement);
+  
+      const authorElement = document.createElement('p');
+      authorElement.textContent = `Author: ${book.author}`;
+      card.appendChild(authorElement);
+  
+      const pagesElement = document.createElement('p');
+      pagesElement.textContent = `Pages: ${book.pages}`;
+      card.appendChild(pagesElement);
+  
+      const statusElement = document.createElement('p');
+      statusElement.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+      card.appendChild(statusElement);
+  
+
+  
+      libraryContainer.appendChild(card);
+    }
+  }
+  
+  
 
