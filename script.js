bff1d98f6f9e6fab2780c5565f998c5fe9b8bdee
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-17 20:24:38 +0530

    Add delete button and style properly

--- script.js
+++ script.js
@@ -1,82 +1,95 @@
+// script.js
+
 const title = document.querySelector('.title');
 const submit = document.querySelector('#submit');
 const author = document.querySelector('.author');
 const pages = document.querySelector('.pages');
 const read = document.querySelector('.status');
+const libraryContainer = document.querySelector('.library-container');
 
 const myLibrary = [];
 
-
-function Book(title,author,pages,read) {
-    
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(title,author,pages,read) {
-    const newBook = new Book(title,author,pages,read);
-    myLibrary.push(newBook)
-    clearDisplay()
-    display();
-    console.log(myLibrary, newBook);
-    }
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  clearDisplay();
+  display();
+}
 
-
-submit.addEventListener('click', function (e){
-    e.preventDefault();
-    if (title.value !== '' && author.value !== ''){
+submit.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (title.value !== '' && author.value !== '') {
     const bookTitle = title.value;
     const authorName = author.value;
     const pagesRead = pages.value;
     const readStatus = read.checked;
 
-    addBookToLibrary(bookTitle,authorName,pagesRead,readStatus)
-    } else{
-        alert('Fill the Required Fields[Title and Author]')
-    }
-})
+    addBookToLibrary(bookTitle, authorName, pagesRead, readStatus);
+  } else {
+    alert('Fill the Required Fields[Title and Author]');
+  }
+});
 
-function clearDisplay(){
-    title.value = ''
-    author.value = ''
-    pages.value = '0'
-    read.checked = ''
+function clearDisplay() {
+  title.value = '';
+  author.value = '';
+  pages.value = '0';
+  read.checked = '';
 }
 
 function display() {
-    const libraryContainer = document.querySelector('.library-container');
-  
+  libraryContainer.innerHTML = '';
 
-    libraryContainer.innerHTML = '';
-  
-    for (const book of myLibrary) {
-      const card = document.createElement('div');
-      card.classList.add('book-card');
-  
-      const titleElement = document.createElement('p');
-      titleElement.textContent = `Title: ${book.title}`;
-      card.appendChild(titleElement);
-  
-      const authorElement = document.createElement('p');
-      authorElement.textContent = `Author: ${book.author}`;
-      card.appendChild(authorElement);
-  
-      const pagesElement = document.createElement('p');
-      pagesElement.textContent = `Pages: ${book.pages}`;
-      card.appendChild(pagesElement);
-  
-      const statusElement = document.createElement('p');
-      statusElement.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
-      card.appendChild(statusElement);
-  
+  for (const book of myLibrary) {
+    const card = document.createElement('div');
+    card.classList.add('book-card');
 
-  
-      libraryContainer.appendChild(card);
-    }
+    const titleElement = document.createElement('p');
+    titleElement.textContent = `Title: ${book.title}`;
+    titleElement.classList.add('titleElement');
+    card.appendChild(titleElement);
+
+    const authorElement = document.createElement('p');
+    authorElement.textContent = `Author: ${book.author}`;
+    authorElement.classList.add('authorElement');
+    card.appendChild(authorElement);
+
+    const pagesElement = document.createElement('p');
+    pagesElement.textContent = `Pages: ${book.pages}`;
+    pagesElement.classList.add('pagesElement');
+    card.appendChild(pagesElement);
+
+    const statusElement = document.createElement('p');
+    statusElement.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+    statusElement.classList.add('statusElement');
+    card.appendChild(statusElement);
+
+    const deleteButtonContainer = document.createElement('div');
+    deleteButtonContainer.classList.add('deleteButtonContainer');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '×';
+    deleteButton.classList.add('deleteButton');
+    deleteButton.addEventListener('click', function () {
+      deleteCard(card);
+    });
+
+    deleteButtonContainer.appendChild(deleteButton);
+    card.appendChild(deleteButtonContainer);
+
+    libraryContainer.appendChild(card);
   }
-  
-  
+}
 
+function deleteCard(card) {
+  const index = Array.from(libraryContainer.children).indexOf(card);
+  myLibrary.splice(index, 1);
+  display(); 
+}
