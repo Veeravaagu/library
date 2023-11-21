596111e72e285e69678a870c9ae8943bbd07f84d
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-21 15:27:36 +0530

    Refactor code structure, and apply display mixin to Book class

--- script.js
+++ script.js
@@ -11,20 +11,96 @@ const libraryContainer = document.querySelector('.library-container');
 // Array to store book objects
 const myLibrary = [];
 
+// Display mixin for common display-related functions
+const myDisplayMixin = {
+
+  // Function to clear form input fields
+  clearDisplay() {
+    title.value = '';
+    author.value = '';
+    pages.value = '0';
+    read.checked = '';
+  },
+
+  // Function to display books in the library
+  display() {
+    libraryContainer.innerHTML = '';
+
+    for (const book of myLibrary) {
+      const card = document.createElement('div');
+      card.classList.add('book-card');
+
+      // Title
+      const titleElement = document.createElement('p');
+      titleElement.textContent = `Title: ${book.title}`;
+      titleElement.classList.add('titleElement');
+      card.appendChild(titleElement);
+
+      // Author
+      const authorElement = document.createElement('p');
+      authorElement.textContent = `Author: ${book.author}`;
+      authorElement.classList.add('authorElement');
+      card.appendChild(authorElement);
+
+      // Pages
+      const pagesElement = document.createElement('p');
+      pagesElement.textContent = `Pages: ${book.pages}`;
+      pagesElement.classList.add('pagesElement');
+      card.appendChild(pagesElement);
+
+      // Read status
+      const statusElement = document.createElement('p');
+      statusElement.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+      statusElement.classList.add('statusElement');
+      card.appendChild(statusElement);
+
+      // Delete button
+      const deleteButtonContainer = document.createElement('div');
+      deleteButtonContainer.classList.add('deleteButtonContainer');
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = '×';
+      deleteButton.classList.add('deleteButton');
+      deleteButton.addEventListener('click', function () {
+        deleteCard(card);
+      });
+
+      deleteButtonContainer.appendChild(deleteButton);
+      card.appendChild(deleteButtonContainer);
+
+      libraryContainer.appendChild(card);
+    }
+  },
+
+};
+
 // Book constructor
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+// Apply the mixin to the class
+Object.assign(Book.prototype, myDisplayMixin);
+
+// Function to delete a book card
+function deleteCard(card) {
+  const index = Array.from(libraryContainer.children).indexOf(card);
+  myLibrary.splice(index, 1);
+  myDisplayMixin.display();
 }
 
 // Function to add a book to the library
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  clearDisplay();
-  display();
+  newBook.clearDisplay();
+  newBook.display();
 }
 
 // Event listener for the submit button
@@ -41,68 +117,3 @@ submit.addEventListener('click', function (e) {
     alert('Fill the Required Fields[Title and Author]');
   }
 });
-
-// Function to clear the form input fields
-function clearDisplay() {
-  title.value = '';
-  author.value = '';
-  pages.value = '0';
-  read.checked = '';
-}
-
-// Function to display books in the library
-function display() {
-  libraryContainer.innerHTML = '';
-
-  for (const book of myLibrary) {
-    const card = document.createElement('div');
-    card.classList.add('book-card');
-
-    // Title
-    const titleElement = document.createElement('p');
-    titleElement.textContent = `Title: ${book.title}`;
-    titleElement.classList.add('titleElement');
-    card.appendChild(titleElement);
-
-    // Author
-    const authorElement = document.createElement('p');
-    authorElement.textContent = `Author: ${book.author}`;
-    authorElement.classList.add('authorElement');
-    card.appendChild(authorElement);
-
-    // Pages
-    const pagesElement = document.createElement('p');
-    pagesElement.textContent = `Pages: ${book.pages}`;
-    pagesElement.classList.add('pagesElement');
-    card.appendChild(pagesElement);
-
-    // Read status
-    const statusElement = document.createElement('p');
-    statusElement.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
-    statusElement.classList.add('statusElement');
-    card.appendChild(statusElement);
-
-    // Delete button
-    const deleteButtonContainer = document.createElement('div');
-    deleteButtonContainer.classList.add('deleteButtonContainer');
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = '×';
-    deleteButton.classList.add('deleteButton');
-    deleteButton.addEventListener('click', function () {
-      deleteCard(card);
-    });
-
-    deleteButtonContainer.appendChild(deleteButton);
-    card.appendChild(deleteButtonContainer);
-
-    libraryContainer.appendChild(card);
-  }
-}
-
-// Function to delete a book card
-function deleteCard(card) {
-  const index = Array.from(libraryContainer.children).indexOf(card);
-  myLibrary.splice(index, 1);
-  display();
-}
